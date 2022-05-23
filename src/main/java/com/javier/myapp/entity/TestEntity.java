1d59cca5951e7fb065e62aeaa57394b082fc4acd
JavierSistemas <jreyes.sistemas@outlook.com>
2022-05-23 16:34:23 -0500

    Nombre del proyecto: my-app, Descripción: prueba

---
+++ src/main/java/com/javier/myapp/entity/TestEntity.java
@@ -0,0 +1,16 @@
+package com.javier.myapp.entity;
+
+public class TestEntity {
+	
+	private String nombre;
+
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	
+	
+	
+}
